4c1eb3819ec80b28cddfdd8961756587c3725fc8
Emil Billberg <emil.billberg@apegroup.com>
2017-06-26 20:44:09 +0200

    fix: better implementation

--- focus-ring.js
+++ focus-ring.js
@@ -1,52 +1,51 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  var clickWhitelist = [
-    'input:not([type])',
-    'input[type=text]',
-    'input[type=search]',
-    'input[type=url]',
-    'input[type=tel]',
-    'input[type=email]',
-    'input[type=password]',
-    'input[type=number]',
-    'input[type=date]',
-    'input[type=month]',
-    'input[type=week]',
-    'input[type=time]',
-    'input[type=datetime]',
-    'input[type=datetime-local]',
-    'textarea',
-    '[role=textbox]',
-  ].join(',');
+  var inputWhiteList = {
+    'text': true,
+    'search': true,
+    'url': true,
+    'tel': true,
+    'email': true,
+    'password': true,
+    'number': true,
+    'date': true,
+    'month': true,
+    'week': true,
+    'time': true,
+    'datetime': true,
+    'datetime-local': true,
+  };
 
   function onKeyDown(event) {
     requestAnimationFrame(function() {
       var activeElement = findActiveElement();
-      activeElement.addEventListener('blur', onBlur, true);
       addFocusRingAttribute(activeElement);
     });
   }
 
   function onClick(event) {
     var target = (typeof event.composedPath === 'function') ? event.composedPath()[0] : document.activeElement;
-    var matches = target.matches(clickWhitelist);
+    var matches = matchesWhiteList(target);
     if (matches) {
       requestAnimationFrame(function() {
-        target.addEventListener('blur', onBlur, true);
         addFocusRingAttribute(target);
       });
     }
   }
 
   function onBlur(event) {
-    this.removeEventListener('blur', onBlur, false);
     removeFocusRingAttribute(event.target);
+  }
+
+  function onFocus(event) {
+    var activeElement = findActiveElement();
+    addFocusRingAttribute(activeElement);
   }
 
   function addFocusRingAttribute(element) {
     var hasFocusringAttribute = element.hasAttribute('focus-ring');
     var isBodyElement = element === document.body;
     if (!hasFocusringAttribute && !isBodyElement) {
+      element.addEventListener('blur', onBlur, true);
       element.setAttribute('focus-ring', '');
     }
   }
@@ -54,6 +53,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function removeFocusRingAttribute(element) {
     var hasFocusringAttribute = element.hasAttribute('focus-ring');
     if (hasFocusringAttribute) {
+      element.removeEventListener('blur', onBlur, true);
       element.removeAttribute('focus-ring');
     }
   }
@@ -66,7 +66,26 @@ document.addEventListener('DOMContentLoaded', function() {
     return activeElement;
   }
 
+  function matchesWhiteList(target) {
+    var type = target.type;
+    var tagName = target.tagName;
+
+    if (tagName == 'INPUT' && inputWhiteList[type] && !target.readonly) {
+      return true;
+    }
+
+    if (tagName == 'TEXTAREA' && !target.readonly) {
+      return true
+    }
+
+    if (target.contentEditable == 'true') {
+      return true;
+    }
+
+    return false;
+  }
+
   document.body.addEventListener('keydown', onKeyDown, true);
   document.body.addEventListener('click', onClick, true);
-
+  window.addEventListener('focus', onFocus, true);
 });
